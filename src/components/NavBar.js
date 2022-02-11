40ec52050ceb59091769769d5aba4971e822579f
Lionel Mason <capitalcookhouse@gmail.com>
2022-02-10 20:09:00 -0500

    ListandKeys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,18 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+     <nav>
+        {links.map((link, index)=> <a key={index} href={`#${link}`}> {link} </a>) }
+        {/* {links.map(link => { 
+          link = link+"!"
+          return <a href={`#${link}`}> {link} </a>
+        }) } */}
+        {/* {[<a href="#home">home</a>, <a href="#about">about</a>, <a href="#projects">projects</a>]} */}
+     </nav>
+  )      
+// <a key={somethingForYouToFigureOut} href={somethingForYouToFigureOut}>{link}</a>
+  
 }
 
 export default NavBar;
